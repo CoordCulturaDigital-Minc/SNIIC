bba9f20ac3f44ad223095aeb30a1b19c88255a93
Tiago Mergulhão <me@tmergulhao.com>
2015-08-24 10:09:16 -0300

    Simple 404 page

--- 404.php
+++ 404.php
@@ -1,48 +1,26 @@
-<?php
-/**
- * The template for displaying 404 pages (Not Found).
- *
- * @package WordPress
- * @subpackage simpleX
- * @since simpleX 2.0
- */
+<?php get_header(); ?>
 
-get_header(); ?>
+<div class="section-wrap wrap">
+<section class="row center-block _404">
+	
+	<article class="
+		col-sm-8 col-sm-offset-2
+		col-xs-12 col-xs-offset-0
+		not-found
+		">
+		<h1>404.
+			<small>Página não encontrada.</small>
+		</h1>
+	</article>
+	
+	<?php
+		while ( have_posts() ) {
+			the_post();
+			get_template_part( 'content', get_post_format() );
+		}
+	?>
 
-	<div id="primary">
-		<div id="content" role="main">
-
-			<article id="post-0" class="post error404 not-found">
-				<header class="entry-header">
-					<h2 class="entry-title"><?php _e( 'Well this is somewhat embarrassing, isn&rsquo;t it?', 'simplex' ); ?></h2>
-				</header>
-
-				<div class="entry-content">
-					<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching, or one of the links below, can help.', 'simplex' ); ?></p>
-
-					<?php get_search_form(); ?>
-
-					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
-
-					<div class="widget">
-						<h2 class="widgettitle"><?php _e( 'Most Used Categories', 'simplex' ); ?></h2>
-						<ul>
-						<?php wp_list_categories( array( 'orderby' => 'count', 'order' => 'DESC', 'show_count' => 1, 'title_li' => '', 'number' => 10 ) ); ?>
-						</ul>
-					</div>
-
-					<?php
-					/* translators: %1$s: smilie */
-					$archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'simplex' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-					?>
-
-					<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
-
-				</div><!-- .entry-content -->
-			</article><!-- #post-0 -->
-
-		</div><!-- #content -->
-	</div><!-- #primary -->
+</section>
+</div>
 
 <?php get_footer(); ?>
