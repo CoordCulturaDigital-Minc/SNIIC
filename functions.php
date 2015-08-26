02a21d2e1bb34bf7976c6bc60e2c7a61f551a625
Tiago Mergulhão <me@tmergulhao.com>
2015-08-26 16:28:59 -0300

    HTML5 captions and SNIIC translation domain

--- functions.php
+++ functions.php
@@ -1,1 +1,15 @@
 <?php
+
+if ( ! function_exists( 'SNIIC_setup' ) ) :
+
+function SNIIC_setup() {
+	add_theme_support( 'html5', array(
+		'search-form', 'comment-form', 'comment-list', 'gallery', 'caption'
+	) );
+	add_theme_support( 'post-formats', array(
+		'aside', 'image', 'video', 'quote', 'link', 'gallery', 'status', 'audio', 'chat'
+	) );
+}
+endif;
+
+add_action( 'after_setup_theme', 'SNIIC_setup' );
