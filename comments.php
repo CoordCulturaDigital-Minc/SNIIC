53d333ca69d898b75491077b1b37f3e509ecc374
Tiago Mergulhão <me@tmergulhao.com>
2015-09-02 15:11:50 -0300

    Comment-area

--- comments.php
+++ comments.php
@@ -1,65 +1,40 @@
-<?php do_action( 'simplex_before_comments' ); ?>
-<div id="comments">
-<?php if ( post_password_required() ) : ?>
-	<p class="nopassword"><?php _e( 'This post is password protected. Enter the password to view any comments.', 'simplex' ); ?></p>
-</div><!-- #comments -->
 <?php
-		/* Stop the rest of comments.php from being processed,
-		 * but don't kill the script entirely -- we still have
-		 * to fully load the template.
-		 */
-		return;
-	endif;
+if ( post_password_required() ) {
+	return;
+}
 ?>
 
-<?php // You can start editing here -- including this comment! ?>
+<div id="comments" class="comments-area">
 
-<?php if ( have_comments() ) : ?>
-	<h2 id="comments-title">
-		<?php
-			printf( _n( 'One comment', '%1$s comments', get_comments_number(), 'simplex' ),
-				number_format_i18n( get_comments_number() ) );
-		?>
-	</h2>
+	<?php if ( have_comments() ) : ?>
+	
+		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
+		<nav id="comment-nav-above">
+			<h2 class="assistive-text"><?php _e( 'Comment navigation', 'simplex' ); ?></h2>
+			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'simplex' ) ); ?></div>
+			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'simplex' ) ); ?></div>
+		</nav>
+		<?php endif; ?>
 
-	<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
-	<nav id="comment-nav-above">
-		<h2 class="assistive-text"><?php _e( 'Comment navigation', 'simplex' ); ?></h2>
-		<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'simplex' ) ); ?></div>
-		<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'simplex' ) ); ?></div>
-	</nav>
-	<?php endif; // check for comment navigation ?>
+		<ol class="comment-list">
+			<?php
+				wp_list_comments( array(
+					'style'       => 'ol',
+					'short_ping'  => true,
+					'avatar_size' => 0,
+				) );
+			?>
+		</ol><!-- .comment-list -->
 
-	<ol class="commentlist">
-		<?php
-			/* Loop through and list the comments. Tell wp_list_comments()
-			 * to use simplex_comment() to format the comments.
-			 * If you want to overload this in a child theme then you can
-			 * define simplex_comment() and that will be used instead.
-			 * See simplex_comment() in simpleX/functions.php for more.
-			 */
-			wp_list_comments( array( 'callback' => 'simplex_comment', 'type' => 'comment' ) );
-		?>
-	</ol>
+	<?php endif; ?>
 
-	<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
-	<nav id="comment-nav-below">
-		<h2 class="assistive-text"><?php _e( 'Comment navigation', 'simplex' ); ?></h2>
-		<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'simplex' ) ); ?></div>
-		<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'simplex' ) ); ?></div>
-	</nav>
-	<?php endif; // check for comment navigation ?>
+	<?php
+		// If comments are closed and there are comments, let's leave a little note, shall we?
+		if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
+	?>
+		<p class="no-comments">Comments are closed.</p>
+	<?php endif; ?>
 
-<?php
-	/* If there are no comments and comments are closed, let's leave a little note, shall we?
-	 * But we don't want the note on pages or post types that do not support comments.
-	 */
-	elseif ( ! comments_open() && ! is_page() && post_type_supports( get_post_type(), 'comments' ) ) :
-?>
-	<p class="nocomments"><?php _e( 'Comments are closed.', 'simplex' ); ?></p>
-<?php endif; ?>
+	<?php comment_form(); ?>
 
-<?php comment_form(); ?>
-
-</div><!-- #comments -->
-<?php do_action( 'simplex_after_sidebar' ); ?>
+</div>
