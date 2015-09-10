902c22c33cd63e1fca02b930d1fc9d0d046752a6
Tiago Mergulhão <me@tmergulhao.com>
2015-09-10 15:14:45 -0300

    Navigation bar that actually works
    Sort of…

--- includes/navigator.php
+++ includes/navigator.php
@@ -1,5 +1,8 @@
-<nav class="row center-block clearfix">
-	<?php wp_nav_menu( array( 'theme_location' => 'primary', 'show_home' => false, 'menu_class' => 'col-xs-12 col-sm-8' ) ); ?>
+<div id="navbar" class="navbar">
+<nav class="row center-block clearfix navigation main-navigation" id="site-navigation" role="navigation">
+	<button class="menu-toggle">Menu</button>
+	<?php wp_nav_menu( array( 'theme_location' => 'primary', 'show_home' => false, 'menu_class' => 'nav-menu col-xs-12 col-sm-8', 'menu_id' => 'primary-menu' ) ); ?>
 
 	<?php get_search_form(); ?>
 </nav>
+</div><!-- #navbar -->
